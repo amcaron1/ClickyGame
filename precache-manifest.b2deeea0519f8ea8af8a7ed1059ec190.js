69ef94631548eabc2999a1855b016e3454e9e40f
Andre <amcaron1@msn.com>
2018-11-19 15:07:37 -0600

    Updates

---
+++ precache-manifest.b2deeea0519f8ea8af8a7ed1059ec190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "89f85df2f4a9bf34e5ca",
+    "url": "/ClickyGame/static/js/main.89f85df2.chunk.js"
+  },
+  {
+    "revision": "a1657e7cd1dc52ab5280",
+    "url": "/ClickyGame/static/js/1.a1657e7c.chunk.js"
+  },
+  {
+    "revision": "89f85df2f4a9bf34e5ca",
+    "url": "/ClickyGame/static/css/main.781842f7.chunk.css"
+  },
+  {
+    "revision": "494d37a1b792cbebe0aff85d85d5c15a",
+    "url": "/ClickyGame/index.html"
+  }
+];
