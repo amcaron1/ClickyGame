1f8bf07214eacecc416f055f59e27c58a764bc57
Andre <amcaron1@msn.com>
2018-11-19 21:24:37 -0600

    Updates

---
+++ precache-manifest.6e6a7c4e75957bf196ff19a28466f0ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df87b559bb6204535129",
+    "url": "/static/js/main.df87b559.chunk.js"
+  },
+  {
+    "revision": "e67643045b21357ff2e1",
+    "url": "/static/js/1.e6764304.chunk.js"
+  },
+  {
+    "revision": "df87b559bb6204535129",
+    "url": "/static/css/main.8f2abd3f.chunk.css"
+  },
+  {
+    "revision": "5c9d6afddadbef952ea018d46546bf41",
+    "url": "/index.html"
+  }
+];
